527a8b87ec07912c05f90782366f1fbf2ac00899
p-sahas <eap.sahas@gmail.com>
2025-11-04 10:24:16 +0530

    added model training file

--- pipelines/training_pipeline.py
+++ pipelines/training_pipeline.py
@@ -16,9 +16,9 @@ from model_training import ModelTrainer
 from model_evaluation import ModelEvaluator
 from model_building import XGboostModelBuilder, RandomForestModelBuilder
 sys.path.append(os.path.join(os.path.dirname(__file__), '..', 'utils'))
-from mlflow_utils import MLflowTracker, setup_mlflow_autolog, create_mlflow_run_tags
+#from mlflow_utils import MLflowTracker, setup_mlflow_autolog, create_mlflow_run_tags
 from config import get_model_config, get_data_paths
-import mlflow
+#import mlflow
 logging.basicConfig(level=logging.INFO, format=
     '%(asctime)s - %(name)s - %(levelname)s - %(message)s')
 logger = logging.getLogger(__name__)
---
+++ src/model_training.py
@@ -0,0 +1,36 @@
+import os
+import joblib
+import logging
+import time
+from typing import Any, Tuple, Union
+import numpy as np
+import pandas as pd
+from sklearn.base import BaseEstimator
+
+
+class ModelTrainer:
+    def train(
+            self,
+            model,
+            X_train,
+            Y_train
+            ):
+        model.fit(X_train,
+                  Y_train
+                  )
+        train_score = model.score(X_train, Y_train)
+        return model, train_score
+    
+    def save_model(self, model, filepath):
+        joblib.dump(filepath)
+
+    def load_model(self, filepath):
+        return joblib.load(filepath) 
+
+
+
+
+    
+
+        
+    
