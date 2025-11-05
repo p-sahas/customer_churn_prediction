8602570d47652801ee7c34c6efac6e869023546f
p-sahas <eap.sahas@gmail.com>
2025-11-05 16:28:20 +0530

    build model inference

--- src/model_inference.py
+++ src/model_inference.py
@@ -38,8 +38,10 @@ logger = logging.getLogger(__name__)
 class ModelInference:
     def __init__(self, model_path: str):
         self.model_path = model_path
-        self.model = None
         self.encoders = {}
+        self.load_model()
+        self.binning_config = get_binning_config()
+        self.encoding_config = get_encoding_config()
 
     def load_model(self):
         if not os.path.exists(self.model_path):
@@ -56,13 +58,38 @@ class ModelInference:
     def preprocess_input(self, data):
         data = pd.DataFrame([data])
 
-
         for col, encoder in self.encoders.items():
             data[col] = data[col].map(encoder)
 
-            return data
+        binning = CustomBinningStratergy(self.binning_config['credit_score_bins'])
+        data = binning.bin_feature(data, 'CreditScore')
+
+        ordinal_strategy = OrdinalEncodingStratergy(self.encoding_config['ordinal_mappings'])
+        data = ordinal_strategy.encode(data)
+
+        data = data.drop(columns=['RowNumber', 'CustomerId', 'Firstname', 'Lastname'])
+        return data
+        
+    def predict(self, data):
+        pp_data = self.preprocess_input(data)
+        Y_pred = self.model.predict(pp_data)
+        Y_proba = float(self.model.predict_proba(pp_data)[:, 1])
+
+
+        Y_pred = 'Churn' if Y_pred == 1 else'Retain'
+        Y_proba = round(Y_proba * 100, 2)
+
+        return {
+                "Status" : Y_pred,
+                "Confidence" : f"{Y_proba} %"
+                }
+
         
 data = {
+  "RowNumber": 1,
+  "CustomerId": 15634602,
+  "Firstname": "Grace",
+  "Lastname": "Williams",
   "CreditScore": 619,
   "Geography": "France",
   "Gender": "Female",
@@ -73,11 +100,10 @@ data = {
   "HasCrCard": 1,
   "IsActiveMember": 1,
   "EstimatedSalary": 101348.88,
-  "Exited": 1
 }
 
 inference = ModelInference('artifacts/models/churn_analysis.joblib')
 inference.load_encoders('artifacts/encode')
 
-data = inference.preprocess_input(data)
+data = inference.predict(data)
 print(data)
